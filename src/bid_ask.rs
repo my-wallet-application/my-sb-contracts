dc4d9d0c20cfb421d41c0bdcb8bf6bb87635c177
amigin <amigin@gmail.com>
2024-09-10 13:05:18 +0300

    Added SrcId

--- src/bid_ask.rs
+++ src/bid_ask.rs
@@ -7,11 +7,13 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 pub struct BidAskSbModel {
     #[prost(string, tag = "1")]
     pub id: ::prost::alloc::string::String,
-    #[prost(int64, tag = "2")]
+    #[prost(string, tag = "2")]
+    pub src_id: ::prost::alloc::string::String,
+    #[prost(int64, tag = "3")]
     pub timestamp: i64,
-    #[prost(double, tag = "3")]
-    pub bid: f64,
     #[prost(double, tag = "4")]
+    pub bid: f64,
+    #[prost(double, tag = "5")]
     pub ask: f64,
 }
 
