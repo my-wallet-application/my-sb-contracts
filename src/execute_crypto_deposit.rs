6aa3ea8c58345c0985e7affb03c04bd1598a09a4
amigin <amigin@gmail.com>
2024-10-01 11:09:29 +0300

    Fixes with Crypto-deposit

--- src/execute_crypto_deposit.rs
+++ src/execute_crypto_deposit.rs
@@ -10,8 +10,10 @@ pub struct ExecuteCryptoDepositSbModel {
     pub client_id: String,
     #[prost(string, tag = "3")]
     pub asset_id: String,
-    #[prost(string, tag = "4")]
-    pub transaction_id: String,
-    #[prost(sint64, tag = "5")]
+    #[prost(double, tag = "4")]
+    pub amount: f64,
+    #[prost(string, tag = "5")]
+    pub blockchain_transaction_id: String,
+    #[prost(sint64, tag = "6")]
     pub timestamp: i64,
 }
