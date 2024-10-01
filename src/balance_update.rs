83258e1e58cbeb4bfda7f309b0514a0c5e4f0ba1
amigin <amigin@gmail.com>
2024-10-01 14:44:38 +0300

    Made Improvements about crypto_deposit_operation

--- src/balance_update.rs
+++ src/balance_update.rs
@@ -1,11 +1,13 @@
 use service_sdk::my_service_bus;
 use service_sdk::my_service_bus::macros::my_sb_entity_protobuf_model;
 
-use crate::ContextSbModel;
+use crate::{ContextSbModel, ExecuteCryptoDepositSbModel};
 
 pub const TYPE_CTX_KEY: &str = "type";
 pub const CRYPTO_DEPOSIT_TYPE_CTX_VALUE: &str = "crypto-deposit";
 pub const TX_ID_CTX_KEY: &str = "tx_id";
+pub const CRYPTO_DEPOSIT_ADDRESS_KEY: &str = "deposit_address";
+pub const CRYPTO_DEPOSIT_ADDRESS_MEMO_KEY: &str = "deposit_address_memo";
 
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[my_sb_entity_protobuf_model(topic_id = "balance-update")]
@@ -39,17 +41,15 @@ pub struct BalanceUpdateSbModel {
 impl BalanceUpdateNotificationSbModel {
     pub fn fill_ctx_as_crypto_deposit_operation(
         &mut self,
-        client_id: String,
-        wallet_id: String,
-        delta: f64,
+        model: &ExecuteCryptoDepositSbModel,
         balance: f64,
-        blockchain_transaction_id: String,
     ) {
+        let address = model.deposit_address.as_ref().unwrap();
         self.updates.push(BalanceUpdateSbModel {
-            wallet: wallet_id,
+            wallet: model.asset_id.to_string(),
             balance,
-            delta,
-            client_id,
+            delta: model.amount,
+            client_id: model.client_id.to_string(),
             ctx: vec![
                 ContextSbModel {
                     key: TYPE_CTX_KEY.to_string(),
@@ -57,7 +57,15 @@ impl BalanceUpdateNotificationSbModel {
                 },
                 ContextSbModel {
                     key: TX_ID_CTX_KEY.to_string(),
-                    value: blockchain_transaction_id,
+                    value: model.blockchain_transaction_id.to_string(),
+                },
+                ContextSbModel {
+                    key: CRYPTO_DEPOSIT_ADDRESS_KEY.to_string(),
+                    value: address.address.to_string(),
+                },
+                ContextSbModel {
+                    key: CRYPTO_DEPOSIT_ADDRESS_MEMO_KEY.to_string(),
+                    value: address.memo.to_string(),
                 },
             ],
         });
@@ -71,6 +79,9 @@ impl BalanceUpdateNotificationSbModel {
         let first_el = &self.updates.get(0).unwrap();
         let mut is_crypto_deposit = false;
         let mut tx_id = None;
+
+        let mut crypto_deposit_address = None;
+        let mut crypto_deposit_address_memo = None;
         for ctx in &first_el.ctx {
             if ctx.key == TYPE_CTX_KEY && ctx.value == CRYPTO_DEPOSIT_TYPE_CTX_VALUE {
                 is_crypto_deposit = true;
@@ -79,16 +90,33 @@ impl BalanceUpdateNotificationSbModel {
             if ctx.key == TX_ID_CTX_KEY {
                 tx_id = Some(ctx.value.as_str());
             }
+
+            if ctx.key == CRYPTO_DEPOSIT_ADDRESS_KEY {
+                crypto_deposit_address = Some(ctx.value.as_str());
+            }
+
+            if ctx.key == CRYPTO_DEPOSIT_ADDRESS_KEY {
+                crypto_deposit_address_memo = Some(ctx.value.as_str());
+            }
         }
 
         if is_crypto_deposit {
             if let Some(tx_id) = tx_id {
+                if crypto_deposit_address.is_none() {
+                    panic!("Crypto deposit address is missing {:?}", self);
+                }
+
+                if crypto_deposit_address_memo.is_none() {
+                    panic!("Crypto deposit address memo is missing {:?}", self);
+                }
                 return CryptoDepositInfo {
                     client_id: &first_el.client_id,
                     delta: first_el.delta,
                     balance: first_el.balance,
                     tx_id,
                     wallet_id: &first_el.wallet,
+                    crypto_deposit_address: crypto_deposit_address.unwrap(),
+                    crypto_deposit_address_memo: crypto_deposit_address_memo.unwrap(),
                 }
                 .into();
             }
@@ -104,4 +132,6 @@ pub struct CryptoDepositInfo<'s> {
     pub balance: f64,
     pub tx_id: &'s str,
     pub wallet_id: &'s str,
+    pub crypto_deposit_address: &'s str,
+    pub crypto_deposit_address_memo: &'s str,
 }
--- src/execute_crypto_deposit.rs
+++ src/execute_crypto_deposit.rs
@@ -14,10 +14,20 @@ pub struct ExecuteCryptoDepositSbModel {
     pub asset_id: String,
     #[prost(double, tag = "5")]
     pub amount: f64,
-    #[prost(string, tag = "6")]
+    #[prost(message, tag = "6")]
+    pub deposit_address: Option<CryptoDepositAddressGrpcModel>,
+    #[prost(string, tag = "7")]
     pub blockchain_transaction_id: String,
-    #[prost(sint64, tag = "7")]
+    #[prost(sint64, tag = "8")]
     pub timestamp: i64,
-    #[prost(string, tag = "8")]
+    #[prost(string, tag = "9")]
     pub who: String,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CryptoDepositAddressGrpcModel {
+    #[prost(string, tag = "1")]
+    pub address: String,
+    #[prost(string, tag = "2")]
+    pub memo: String,
 }
