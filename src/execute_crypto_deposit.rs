26b965114b8b935daed5ca0b838ddc9d888f3647
amigin <amigin@gmail.com>
2024-10-01 11:06:40 +0300

    Fixes

--- src/execute_crypto_deposit.rs
+++ src/execute_crypto_deposit.rs
@@ -7,9 +7,11 @@ pub struct ExecuteCryptoDepositSbModel {
     #[prost(string, tag = "1")]
     pub process_id: String,
     #[prost(string, tag = "2")]
-    pub asset_id: String,
+    pub client_id: String,
     #[prost(string, tag = "3")]
+    pub asset_id: String,
+    #[prost(string, tag = "4")]
     pub transaction_id: String,
-    #[prost(sint64, tag = "4")]
+    #[prost(sint64, tag = "5")]
     pub timestamp: i64,
 }
