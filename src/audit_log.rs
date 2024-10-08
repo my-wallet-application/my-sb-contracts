b5ffb7636cc54707130fd77f699b6da3960784a4
amigin <amigin@gmail.com>
2024-10-08 13:28:33 +0300

    Fixed With AuditLog

--- src/audit_log.rs
+++ src/audit_log.rs
@@ -6,15 +6,13 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 #[derive(Serialize, Deserialize)]
 pub struct AuditLogSbContract {
     #[prost(string, tag = "1")]
-    pub id: String,
-    #[prost(uint64, tag = "2")]
-    pub moment: u64,
+    pub client_id: String,
+    #[prost(int64, tag = "2")]
+    pub moment: i64,
     #[prost(string, tag = "3")]
-    pub message: String,
-    #[prost(string, tag = "4")]
-    pub label: String,
-    #[prost(string, tag = "5")]
     pub who: String,
+    #[prost(string, tag = "4")]
+    pub message: String,
     #[prost(string, tag = "6")]
-    pub ctx: String,
+    pub tech_data: String,
 }
